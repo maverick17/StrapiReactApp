e142f8a1483a072ac85378f25b3479eb6ce5b903
Amit Amritsagar <aamritsagar@Amits-MacBook-Pro-Retina.local>
2023-06-12 00:10:44 +0530

    initial commit

---
+++ backend/config/plugins.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    //
+    graphql: {
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: false,
+        depthLimit: 10,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+        },
+      },
+    },
+  };
